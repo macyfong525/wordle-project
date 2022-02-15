aab9c1a2cd4bf5b141fe42fff63045d216ce8cf5
Macy Fong <macyfong525@gmail.com>
2022-02-15 05:57:08 -0800

    add duplicate letter use case;

--- wordle-unittest.py
+++ wordle-unittest.py
@@ -14,6 +14,14 @@ class MyTestCase(unittest.TestCase):
         final_word = wordle.get_suggested_words()
         self.assertEqual(final_word, 'robin')  # add assertion here
 
+    def test_duplicate_letter(self):
+        wordle = Wordle()
+        a = 'lists'
+        b = "bybbg"
+        wordle.insert(a, b)
+        word_list = wordle.get_suggested_words()
+        self.assertEqual('criss' in word_list, True)
+
 
 if __name__ == '__main__':
     unittest.main()
